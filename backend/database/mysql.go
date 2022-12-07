122c7d0903c519421012f77244be6772d40706da
Erodotos <edemet01@cs.ucy.ac.cy>
2022-12-07 16:43:05 +0000

    env variables

--- backend/database/mysql.go
+++ backend/database/mysql.go
@@ -20,9 +20,11 @@ var (
 func init() {
 
 	var err error
-	p := config.Config("DB_PORT")
+	// p := config.Config("DB_PORT")
+	p := os.Getenv("DB_PORT")
 	port, _ := strconv.ParseUint(p, 10, 32)
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_HOST"), port, config.Config("DB_NAME"))
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_HOST"), port, config.Config("DB_NAME"))
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), port, os.Getenv("DB_NAME"))
 	fmt.Println(dsn)
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
