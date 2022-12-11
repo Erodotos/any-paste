86e17162d4d8726098c283bdb01106b79539120b
Erodotos <edemet01@cs.ucy.ac.cy>
2022-12-11 11:07:07 +0000

    learning k8s

--- frontend/handlers/post.go
+++ frontend/handlers/post.go
@@ -1,6 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"frontend/models"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,4 +17,21 @@ func ReadPost(c *fiber.Ctx) error {
 	return c.Render("post", fiber.Map{
 		"Post": "Hello, World!",
 	})
+}
+
+func CreatePost(c *fiber.Ctx) error {
+
+	client := fiber.AcquireClient()
+	a := client.Post(os.Getenv("API_URL"))
+	a.JSON(fiber.Map{"post": "123"})
+	code, body, _ := a.Bytes()
+
+	fmt.Println(code)
+
+	post := models.Test{}
+	json.Unmarshal(body, &post)
+
+	fmt.Println(post)
+
+	return c.JSON(post)
 }
